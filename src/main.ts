39b4d588831347ef0a94139f14d88b03841cc04d
João Vitor Olsen <jolsenpessoal@gmail.com>
2023-07-23 19:10:08 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(process.env.MONGO_URI);
 
   app.useGlobalPipes(new ValidationPipe());
 
