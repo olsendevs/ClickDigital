9499431ee027ba51e61bbf87fcd17511e2818092
João Vitor Olsen <joao.vitor@inovaebiz.com.br>
2023-10-18 09:11:29 -0300

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -32,8 +32,7 @@ export class UserController {
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
-  @UseGuards(RolesGuard)
-  @Roles('admin')
+
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
